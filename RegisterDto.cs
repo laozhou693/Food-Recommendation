b6a00270253c0b5d39083ab03f7b1ae5771bd95b
星星不见我 <164742031+hbhkl19@users.noreply.github.com>
2025-05-21 17:41:25 +0800

    Add files via upload

---
+++ RegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRecommendationSystem.Core.Models
+{
+    /// <summary>
+    /// 用户注册请求的数据传输对象（DTO）
+    /// </summary>
+    public class RegisterDto
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
+
